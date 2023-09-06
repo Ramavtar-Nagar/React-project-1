a3afe24634b14ec019f98d35bc084992e49c363c
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2023-09-06 18:30:34 +0530

    Create App.js
    just testing it a little bit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+import React, {useState} from 'react'
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  const toggleMode = () => { 
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743'
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils -Dark Mode";
+      // setInterval(() => {
+      //   document.title = "TextUtils is Amazing";
+      // }, 1500);
+      // setInterval(() => {
+      //   document.title = "Install TextUtils Now";
+      // }, 1000);
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Light MOde";
+    }
+  }
+
+  return (
+    <>
+        {/* <Navbar/> */}
+        <Router>
+          <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
+          <Alert alert={alert}/>
+
+          <div className='container my-3'>
+            <Routes>
+                <Route exact path="/about" element={<About/>}> </Route>
+                <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text below to analyze" mode={mode} />}> </Route>
+            </Routes>
+          </div>
+        </Router>
+    </>
+    );
+}
+
+export default App;
